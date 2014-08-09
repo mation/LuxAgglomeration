d863ebf2b21cd969173c84476c7df846b089e5b5
mation <erszat@gmail.com>
2014-08-09 09:55:25 +0200

    add mainwindow view

---
+++ LuxAgglomerationDBManager/src/view/MainWindow.java
@@ -0,0 +1,14 @@
+package view;
+
+import javax.swing.JFrame;
+
+public class MainWindow extends JFrame {
+	public MainWindow() {
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
